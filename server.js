bf7d1376a6dbac2fa7db4bc4a0ccf0f58baee9d6
Arshad Khan <arshad19ahmadkhan@gmail.com>
2017-09-06 20:22:27 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) =>  {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log + '\n', (err) => {
+    if(err) {
+      console.log('unable to append server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) =>  {
+  res.render('maintainence.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  // res.send('Hello Express');
+  res.render('home.hbs', {
+pageTitle: 'Home Page',
+welcomeMessage: 'Welcome 2'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+app.listen(3000);
