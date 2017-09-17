bccbce87554ea1bf9d4ab453e3ef3d6d8795daf6
Arshad Khan <arshad19ahmadkhan@gmail.com>
2017-09-17 11:52:48 -0400

    Add project page

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ app.get('/about', (req, res) => {
     pageTitle: 'About page'
   });
 });
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
